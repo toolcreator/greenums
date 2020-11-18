be0d25d7fa0be8a22a3f115eabf1ebcbd5b965a4
de.toolcreator <de.toolcreator@gmail.com>
2020-11-18 21:38:36 +0100

    Add options to start at a certain year and month

--- Main.hs
+++ Main.hs
@@ -11,10 +11,8 @@ module Main where
 
 -- base
 import Control.Exception (IOException, catch)
-import System.Environment (getArgs)
 import Data.Maybe (fromMaybe)
 import Data.List (sortBy)
-import Control.Applicative (empty)
 
 -- bytestring
 import qualified Data.ByteString.Lazy  as B
@@ -51,6 +49,9 @@ import Graphics.Dynamic.Plot.R2
 -- color
 import qualified Data.Colour.Names as Color
 
+-- optparse-applicative https://stackoverflow.com/questions/8610277/haskell-parsing-command-line-arguments
+import Options.Applicative
+
 
 data MonthInYear = MonthInYear {
   year :: Integer,
@@ -63,8 +64,8 @@ instance FromField MonthInYear where
                         return (MonthInYear (fst3 g) (snd3 g))
 
 instance Hashable MonthInYear where
-  hashWithSalt s (MonthInYear m y) = s `hashWithSalt` m `hashWithSalt` y `hashWithSalt` m
-
+  hashWithSalt s (MonthInYear y m) = s `hashWithSalt` y `hashWithSalt` m
+  
 data Transaction = Transaction {
   monthInYear :: MonthInYear,
   amount :: Float
@@ -73,12 +74,24 @@ instance FromNamedRecord Transaction where
   parseNamedRecord m = Transaction
                         <$> m .: "Buchungstag"
                         <*> parseAmount m where
-                          parseAmount :: NamedRecord -> Parser Float
+                          parseAmount :: NamedRecord -> Data.Csv.Parser Float
                           parseAmount m = maybe empty (parseGerFloat . B.fromStrict) (HM.lookup "Betrag" m) where
-                            parseGerFloat :: B.ByteString -> Parser Float
+                            parseGerFloat :: B.ByteString -> Data.Csv.Parser Float
                             parseGerFloat str = return (
                               read $ C.unpack $ S.replace "," ("." :: B.ByteString) str :: Float
                               )
+
+data Options = Options {
+  filename :: String,
+  sy :: Integer,
+  sm :: Int
+}
+
+options :: Options.Applicative.Parser Main.Options
+options = Options <$> argument str (metavar "STR" <> help "filename")
+                  <*> argument auto (metavar "INT" <> help "start year (only two digits, starting 2000)" <> showDefault
+                                                                                                         <> value 0)
+                  <*> argument auto (metavar "INT" <> help "start month (1-12)" <> showDefault <> value 1)
 
 decodeTransactions :: B.ByteString -> Either String (Vector Transaction)
 decodeTransactions = fmap snd . decodeByNameWith (DecodeOptions _semicolon)
@@ -93,8 +106,8 @@ catchShowIO action = fmap Right action `catch` handleIOException where
 
 type MonthlySums = HM.HashMap MonthInYear Float
 
-aggregateTransactions :: Vector Transaction -> MonthlySums
-aggregateTransactions transactions = HM.fromList [(MonthInYear y m, s) |
+aggregateTransactions :: Integer -> Int -> Vector Transaction -> MonthlySums
+aggregateTransactions sy sm transactions = HM.fromList [(MonthInYear y m, s) |
     let transactionList = toList transactions,
     let _year = year . monthInYear,
     let _month = month . monthInYear,
@@ -102,9 +115,11 @@ aggregateTransactions transactions = HM.fromList [(MonthInYear y m, s) |
     let ys = map _year transactionList,
     let ms = map _month transactionList,
 
-    (y, m) <- spne $ zip ys ms,
-    let s = sum [amount t | t <- transactionList, y == _year t, m == _month t]
-  ] where
+    (y, m) <- spne $ filter agePred $ zip ys ms,
+    let s = sum [amount t | t <- transactionList, y == _year t, m == _month t]] where
+    -- predicate for transaction to be younger than (sy, sm)
+    agePred :: (Integer, Int) -> Bool
+    agePred (y, m) = y >= sy && m >= sm
     -- Sequentially Pairwise Not Equal, i.e., no two neighbours in the resulting list are the same
     spne :: Eq a => [a] -> [a]
     spne xs = _spne [] xs where
@@ -114,16 +129,19 @@ aggregateTransactions transactions = HM.fromList [(MonthInYear y m, s) |
       _spne res xs = res' ++ _spne res' (tail xs) where
         res' = res ++ [head xs | last res /= head xs]
 
-main = do [filename] <- getArgs
-          transactions <- decodeTransactionsFromFile filename
-          let monthlySums = either (const Nothing) (Just . aggregateTransactions) transactions
-          let plotData = sortBy (\(a,_) (b,_) -> compare a b)
-                [(x :: Double, y :: Double) |
-                  m <- maybe [] HM.keys monthlySums,
-                  let x = fromIntegral (year m) + fromIntegral (month m) / 12.0 :: Double,
-                  let y = floatToDouble $ fromMaybe 0 $ maybe (Just 0) (HM.lookup m) monthlySums
-                ]
-          let total = sum [snd p | p <- plotData]
-          let plot =  tint (if total >= 0 then Color.green else Color.red) $ lineSegPlot plotData
-          print plotData
-          plotWindow [plot]
+greenums (Options filename sy sm) = do
+  transactions <- decodeTransactionsFromFile filename
+  let monthlySums = either (const Nothing) (Just . aggregateTransactions sy sm) transactions
+  let plotData = sortBy (\(a,_) (b,_) -> compare a b)
+                        [(x :: Double, y :: Double) |
+                          m <- maybe [] HM.keys monthlySums,
+                          let x = fromIntegral (year m) + fromIntegral (month m) / 12.0 :: Double,
+                          let y = floatToDouble $ fromMaybe 0 $ maybe (Just 0) (HM.lookup m) monthlySums
+                        ]
+  let total = sum [snd p | p <- plotData]
+  let plot =  tint (if total >= 0 then Color.green else Color.red) $ lineSegPlot plotData
+  print plotData
+  plotWindow [plot]
+
+main = execParser opts >>= greenums where
+  opts = info (helper <*> options) (Options.Applicative.header "greenums")
